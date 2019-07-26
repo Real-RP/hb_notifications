cdf7d50fd80d3f5d29917103b700b6e726fa8eb6
Real-RP <47250861+Real-RP@users.noreply.github.com>
2019-07-26 17:43:16 +0200

    sound added! ;)

--- hb_notifications/html/listener.js
+++ hb_notifications/html/listener.js
@@ -17,25 +17,51 @@ $(function(){
 					
 				} else if (item.position === 'top-left') {
                     $("#message").show(250);
+                    $("#box").show(250);
 				} else if (item.position === 'top-right') {
                     $("#message-top-right").show(250);
+					$("#box").show(250);
                 } else if (item.position === 'top-middle') {
 					$("#message-top-middle").show(250);
+					$("#box").show(250);
 				} else if (item.position === 'middle-middle') {
 					$("#message-middle-middle").show(250);
+					$("#box").show(250);
 				} else if (item.position === 'middle-left') {
 					$("#message-middle-left").show(250);
+					$("#box").show(250);
 				} else if (item.position === 'middle-right') {
 					$("#message-middle-right").show(250);
+					$("#box").show(250);
 				} else if (item.position === 'bottom-middle') {
 					$("#message-bottom-middle").show(250);
+					$("#box").show(250);
 				} else if (item.position === 'bottom-left') {
 					$("#message-bottom-left").show(250);
+					$("#box").show(250);
 				} else if (item.position === 'bottom-right') {
 					$("#message-bottom-right").show(250);
+					$("#box").show(250);
 				} else
 					$("#message").show(250);
 			}
 		});
 	};
 });
+
+
+
+var player = null;
+window.addEventListener('message', function(e) {
+    if (e.data.message == 'play') {
+  		
+	  if (player != null) {
+		player.pause();
+		player.currentTime = 0;
+	  }
+	  player = new Audio("./sound/"+e.data.sound);
+	  player.volume = e.data.volume;
+
+	  player.play();
+	}
+})
